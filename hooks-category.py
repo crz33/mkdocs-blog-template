c1a152031fbd5e69a50b12304b46eb20683e0c22
crz33 <takada.masaharu@gmail.com>
2025-01-20 20:18:40 +0900

    hook category title

---
+++ hooks-category.py
@@ -0,0 +1,30 @@
+import csv
+
+# create category map / url : (slug, display-name)
+categories = {}
+with open("hooks-category.csv") as f:
+    csv_reader = csv.reader(f)
+    for record in csv_reader:
+        categories[f"blog/category/{record[0]}/"] = record
+
+
+def on_pre_page(page, config, files):
+    """
+    change category title
+    """
+    if page.url in categories:
+        page.title = categories[page.url][1]
+    return page
+
+
+def on_page_markdown(markdown, page, config, files):
+    """
+    change category page title
+    """
+    if page.url in categories:
+        print(markdown)
+        markdown = markdown.replace(
+            f"# {categories[page.url][0]}",
+            f"# 「{categories[page.url][1]}」の記事一覧",
+        )
+    return markdown
